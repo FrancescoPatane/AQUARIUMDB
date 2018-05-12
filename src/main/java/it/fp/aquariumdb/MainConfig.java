127e549ce0a577aab9592e6174c449549abe4b0d
francesco <francesco@francesco-HP-Notebook>
2018-05-12 23:13:01 +0200

    p

--- src/main/java/it/fp/aquariumdb/MainConfig.java
+++ src/main/java/it/fp/aquariumdb/MainConfig.java
@@ -18,6 +18,9 @@ public class MainConfig {
 	public DataSource dataSource() throws URISyntaxException {
 
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		
+		//String dburl = "postgres://cntynynccvjert:f13fdadce13c412860e78a710a4058a294432dbd00a1e584acdbc90f04a450e0@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d57ul6jhk8h2gf";
+		//URI dbUri = new URI(dburl);
 		System.out.println("#########################################");
 		System.out.println(dbUri);
 		System.out.println("###########################################");
